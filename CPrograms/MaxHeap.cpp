68760157a206df4d80e495283223fae7b5f085cb
singhania53739 <singhania.53739@gmail.com>
2015-03-31 16:26:04 +0530

    Update MaxHeap.cpp

--- CPrograms/MaxHeap.cpp
+++ CPrograms/MaxHeap.cpp
@@ -28,3 +28,137 @@ int main(){
   heapify(a,i);
   
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+package Others;
+
+public class Heap {
+	int[] a={20,12,5,9,22,51,1};
+	int heapSize;
+
+	void maxHeapify(int a[],int i){
+		int lar=i;
+		int left=(2*i)+1;
+		int right=(2*i)+2;
+		if(left<heapSize && a[left]>a[lar])
+			lar=left;
+		if(right<heapSize && a[right]>a[lar])
+			lar=right;
+		if(lar!=i){
+			int temp=a[i];
+			a[i]=a[lar];
+			a[lar]=temp;
+			maxHeapify(a, lar);
+		}
+	}
+
+	void minHeapify(int a[],int i){
+		int lea=i;
+		int left=(i*2)+1;
+		int right=(i*2)+2;
+		if(left<heapSize && a[left]<a[lea])
+			lea=left;
+		if(right<heapSize && a[right]<a[lea])
+			lea=right;
+		if(lea!=i){
+			int temp=a[lea];
+			a[lea]=a[i];
+			a[i]=temp;
+			minHeapify(a, lea);
+		}
+	}
+
+	public void heapSort(){
+
+		heapSize=a.length;
+		for (int i = (heapSize-2)/2; i >=0; i--)
+			minHeapify(a, i);
+		for (int i = 0; i < a.length; i++)
+			System.out.print(a[i]+" ");
+
+		System.out.println();
+		while(heapSize>1){
+			int temp=a[0];
+			a[0]=a[heapSize-1];
+			a[heapSize-1]=temp;
+			heapSize--;
+			minHeapify(a, 0);
+		}
+		for (int i = 0; i < a.length; i++)
+			System.out.print(a[i]+" ");
+	}
+
+	public int kthLargest(int k){
+		heapSize=a.length;
+		int l=k;
+		for (int i = (heapSize-2)/2; i >=0; i--)
+			minHeapify(a, i);
+
+		for (int i = 0; i < a.length; i++)
+			System.out.print(a[i]+" ");
+
+		System.out.println();
+
+		while(k>0 && heapSize>1){
+			int temp=a[0];
+			a[0]=a[heapSize-1];
+			a[heapSize-1]=temp;
+			heapSize--;
+			k--;
+			minHeapify(a, 0);
+		}
+
+		return a[a.length-l];
+	}
+
+	public void heapBuilder(){
+		heapSize=a.length;
+		for (int i = 0; i < a.length; i++)
+			System.out.print(a[i]+" ");
+
+		System.out.println();
+
+		for(int i=(heapSize-2)/2;i>=0;i--)
+			maxHeapify(a, i);
+
+
+		for (int i = 0; i < a.length; i++)
+			System.out.print(a[i]+" ");
+
+		System.out.println();
+
+		for(int i=(heapSize-2)/2;i>=0;i--)
+			minHeapify(a, i);
+
+		for (int i = 0; i < a.length; i++)
+			System.out.print(a[i]+" ");
+
+	}
+}
+
